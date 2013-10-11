db06ca968614c0d3c9e05a7d26d75fce1da5d6e3
ichimura <ne230212@senshu-u.jp>
2013-10-11 11:48:54 +0900

    SoftDeleteBehaviorの追加処理

--- app/Config/Schema/schema.php
+++ app/Config/Schema/schema.php
@@ -87,7 +87,8 @@ class AppSchema extends CakeSchema {
 		'user_password' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 64, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
-		'dalete_flag' => array('type' => 'integer', 'null' => true, 'default' => '0'),
+		'deleted' => array('type' => 'integer', 'null' => true, 'default' => '0' , 'length' => 1),
+		'deleted_date' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'indexes' => array(
 			'PRIMARY' => array('column' => 'id', 'unique' => 1)
 		),

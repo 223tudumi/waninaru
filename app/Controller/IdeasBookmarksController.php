752174267ed5fdeec476ae7b0d12a202c4f91d5a
ichimura <ne230212@senshu-u.jp>
2013-12-12 17:55:21 +0900

    アイデアの前提

--- app/Controller/IdeasBookmarksController.php
+++ app/Controller/IdeasBookmarksController.php
@@ -1,37 +1,36 @@
 <?php
 class IdeasBookmarksController extends AppController{
 	public $helpers = array('Html' , 'Form');
-	var $uses = array('ProjectsBookmark');
+	var $uses = array('IdeasBookmark');
 	
 	public function beforeFilter(){
 		parent::beforeFilter();
 	}
 	
 	public function index(){
-		/**
 		$userSession = $this->Auth->user();
-		$this->ProjectsBookmark->recursive=3;
+		$this->IdeasBookmark->recursive=3;
 		$this->paginate = array(
 			'conditions' => array('user_id'=>$userSession['id']),
 			'limit' => 9,
 			'order' => array(
-					'Project.created' => 'asc'
+					'Idea.created' => 'asc'
 			)
 		);
 		$this->set('date',$this->paginate());
-		**/
 	}
 	
-	public function regist($project_id=null){
+	public function regist($idea_id=null){
 		$this->autoRender = false;
 		$userSession = $this->Auth->user();
-		$this->request->data['ProjectsBookmark']['project_id'] = $project_id;
-		$this->request->data['ProjectsBookmark']['user_id'] = $userSession['id'];
+		$this->request->data['IdeasBookmark']['idea_id'] = $project_id;
+		$this->request->data['IdeasBookmark']['user_id'] = $userSession['id'];
 		
-		if(!$this->ProjectsBookmark->save($this->request->data)){
+		if(!$this->IdeasBookmark->save($this->request->data)){
 			$this->Session->setFlash('失敗したよ!!!');
 		}
-		$this->redirect(array('controller'=>'projects','action'=>'detail',$project_id));
+		//TODO ここのリダイレクト先がアイデアの場合どこが良いか解らないので江本氏の判断にまかせます
+		$this->redirect(array('controller'=>'ideas','action'=>'detail',$project_id));
 		
 	}
 	
